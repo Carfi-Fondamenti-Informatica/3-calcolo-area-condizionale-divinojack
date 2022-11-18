d47227f8c4d60989920f1040040f65ac9d2bd01f
divinojack <115471906+divinojack@users.noreply.github.com>
2022-11-18 17:46:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   float a=0, b=0, risultato=0;
+    int opzione=0;
+    cin >> a >> b>> opzione;
+    switch (opzione) {
+        case 0:
+            cout << "Risultato=" << (a*b)/2 << endl;
+            break;
+        case 1:
+            cout << "Risultato=" << (a*a) << endl;
+            break;
+        case 2:
+            cout << "Risultato=" << (a*b) << endl;
+            break;
+        default:
+            cout<< "Opzione non valida" << endl;
+    }
    return 0;
 }
